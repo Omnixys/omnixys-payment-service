c9983cf1544ff28bf21ff9a58699fa769bf38186
Caleb-Script <caleb-script@outlook.de>
2025-06-03 14:55:38 +0200

    chore: update

--- src/main/resources/db/migration/V1.0__Create.sql
+++ src/main/resources/db/migration/V1.0__Create.sql
@@ -33,7 +33,7 @@ CREATE TYPE PAYMENTMETHOD AS ENUM (
 
 -- Tabelle zur Speicherung von Zahlungen
 CREATE TABLE IF NOT EXISTS payment (
-    id UUID PRIMARY KEY USING INDEX TABLESPACE paymentspace,
+    id UUID PRIMARY KEY,
     username TEXT NOT NULL,
     account_id UUID NOT NULL,
     amount DECIMAL(8,2) NOT NULL CHECK (amount > 0),
