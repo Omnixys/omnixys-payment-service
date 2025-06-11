a23c04fb9b8eced3626625cb5dc9bc2e81e9fcbc
Caleb-Script <caleb-script@outlook.de>
2025-06-11 14:42:49 +0200

    create test migration

---
+++ src/main/resources/db/migration/test/V1.0__Create.sql
@@ -0,0 +1,46 @@
+-- Enum-Typ für Zahlungsstatus
+CREATE TYPE PAYMENTSTATUS AS ENUM (
+    'PENDING',
+    'PROCESSING',
+    'COMPLETED',
+    'FAILED',
+    'CANCELLED',
+    'REFUNDED'
+    );
+
+-- Enum-Typen für Währung und Zahlungsmethode
+CREATE TYPE CURRENCYTYPE AS ENUM (
+    'EUR',
+    'USD',
+    'GBP',
+    'CHF',
+    'JPY',
+    'CNY',
+    'GHS'
+    );
+
+CREATE TYPE PAYMENTMETHOD AS ENUM (
+    'CREDIT_CARD',
+    'DEBIT_CARD',
+    'PAYPAL',
+    'APPLE_PAY',
+    'GOOGLE_PAY',
+    'BANK_TRANSFER',
+    'BITCOIN'
+    );
+
+-- Tabelle zur Speicherung von Zahlungen
+CREATE TABLE IF NOT EXISTS payment (
+    id UUID PRIMARY KEY,
+    username TEXT NOT NULL,
+    account_id UUID NOT NULL,
+    amount DECIMAL(8,2) NOT NULL CHECK (amount > 0),
+    currency TEXT NOT NULL,
+    method TEXT NOT NULL,
+    status TEXT NOT NULL,
+    invoice_id UUID,
+    created timestamp NOT NULL
+);
+
+-- Indizes für gezielte Abfragen
+CREATE INDEX IF NOT EXISTS payment_invoice_id_idx ON payment(invoice_id);
